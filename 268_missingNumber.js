e2ae1fb53e5f079c6c80cdc7760338ff24e68a0e
qyf381389362 <381389362@qq.com>
2018-05-22 19:25:45 +0800

    创建268题

--- 268_missingNumber.js
+++ 268_missingNumber.js
@@ -1,12 +1,16 @@
-const readline = require('readline');
+const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 rl.on('line', (line) => {
   let nums = line.split(' ');
-  console.log(missingNumber());
-});
+  console.log(missingNumber('[3,0,1,4,5,9]'));
+})
 var missingNumber = function (nums) {
-
-};
+  let new_nums = nums.match(/[^\[,\]]/g);
+  let max = Math.max(...new_nums);
+  // let max = Math.max.apply(null, new_nums);
+  
+  return max;
+}
